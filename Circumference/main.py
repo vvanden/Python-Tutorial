c8a1b947db6023af108961f22d6fa1a0e352e777
vvanden <vvanden@uwo.ca>
2024-01-20 16:40:09 -0500

    adding circumference project

---
+++ Circumference/main.py
@@ -0,0 +1,6 @@
+pi = 3.141592653589793
+radius = float(input("Input radius:"))
+circumference = pi * radius * 2
+area = pi * radius * radius
+print(f"The circumference is: {circumference}")
+print(f"The area is: {area}")
