62873cf01f22ab0b634edaf1d97d818ffc829d58
vvanden <vvanden@uwo.ca>
2024-01-20 16:20:33 -0500

    Adding the calculator project

---
+++ Calculator/main.py
@@ -0,0 +1,16 @@
+first_number = input("Input first number:")
+first_number = float(first_number)
+second_number = input("Input second number: ")
+second_number = float(second_number)
+sum = first_number + second_number
+sum = str(sum)
+subtraction = first_number - second_number
+subtraction = str(subtraction)
+multiplication = first_number * second_number
+multiplication = str(multiplication)
+division = first_number / second_number
+division = str(division)
+print("Sum: " + sum)
+print("Subtraction: " + subtraction)
+print("Multiplication: " + multiplication)
+print("Division: " + division)
