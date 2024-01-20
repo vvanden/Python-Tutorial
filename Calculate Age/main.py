ee171cfa29f577c1ae7c6ee193e2bc10d3f164eb
vvanden <vvanden@uwo.ca>
2024-01-20 16:08:25 -0500

    Added age calculation assignment

---
+++ Calculate Age/main.py
@@ -0,0 +1,7 @@
+start_year = input("What year were you born: ")
+start_year = int(start_year)
+end_year = input("What is the current year: ")
+end_year = int(end_year)
+age = end_year - start_year
+age = str(age)
+print("You are: " + age + " years old.")
