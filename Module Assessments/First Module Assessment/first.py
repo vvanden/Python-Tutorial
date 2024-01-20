cec5721fc5a5cf13480d30fddd26e212b5570406
vvanden <vvanden@uwo.ca>
2024-01-20 14:33:38 -0500

    Update first.py
    Adding requested code to first.py

---
+++ Module Assessments/First Module Assessment/first.py
@@ -0,0 +1,4 @@
+myname = "Vincent"
+myage = "43"
+mylearnsum = "I want to lean how to use Python and be more productive at my work."
+print("My name is %s, and I am %s, I'm hoping to learn %s" % (myname, myage, mylearnsum))
