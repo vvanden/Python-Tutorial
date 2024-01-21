48f9340e6f475f5ed6d2033a7bcfecfa6113e993
vvanden <vvanden@uwo.ca>
2024-01-21 16:42:31 -0500

    New factoral calculation program

---
+++ Calculate the Factorial/main.py
@@ -0,0 +1,8 @@
+usernum = int(input("Input number for Factoral Calculation:"))
+curval = usernum
+factnum = usernum
+while curval > 1:
+    curval-=1
+    factnum = factnum * curval
+
+print(f"The Factoral of {usernum} is {factnum}")
