7983156b72db64c7d8ae06b164fcff647e144f5c
vvanden <vvanden@uwo.ca>
2024-02-01 12:10:06 -0500

    updated merge sort alg

--- Merge Sort/main.py
+++ Merge Sort/main.py
@@ -1,35 +1,48 @@
 def mergesort(msls):
 
-    ls_len=len(msls)
+    if len(msls) > 1:  
 
-    if ls_len > 1:
+        ls_left = msls[:len(msls)//2]
+        ls_right = msls[len(msls)//2:]
 
-        ls_half = int(ls_len / 2)
+        #recursion
+        mergesort(ls_left)
+        mergesort(ls_right)
 
-        ls_s = msls[0:ls_half]
-        ls_e = msls[ls_half:ls_len]
+        L_idx = 0 #leftmost element of LEFT arrary index
+        R_idx = 0 #leftmost element of RIGHT arrary index
+        M_idx = 0 #merged array index
 
-        mergesort(ls_s)
-        mergesort(ls_e)
+        #merge, compare left to right array and push lowest values into merged array
+        while L_idx < len(ls_left) and R_idx < len(ls_right):
+            #left array is lower, push in merged array
+            if ls_left[L_idx] < ls_right[R_idx]:
+                msls[M_idx] = ls_left[L_idx]
+                L_idx += 1
+                M_idx += 1
 
-        msls_len=len(msls)
+            else:
+            #right array is lower, push in merged array
+                msls[M_idx] = ls_right[R_idx]
+                R_idx += 1
+                M_idx += 1
 
-        print(msls)
-
-        for idx2 in range(msls_len - 1):
-            if idx2 < msls_len:
-                if msls[idx2] > msls[idx2 + 1]:
-                    ls_hold = msls[idx2 + 1]
-                    msls[idx2 + 1] = msls[idx2]
-                    msls[idx2] = ls_hold
-
+        #nothing left to compare, place remaining left array items into the merged array
+        while L_idx < len(ls_left):
+            msls[M_idx] = ls_left[L_idx]
+            L_idx += 1
+            M_idx += 1
+        
+        #nothing left to compare, place remaining right array items into the merged array
+        while R_idx < len(ls_right):
+            msls[M_idx] = ls_right[R_idx]
+            R_idx += 1
+            M_idx += 1
 
 ls=[33,88,1,22,99,44,11,66,55,77]
-#ls=[33,88,1,22,99,44,11,66,55]
 
 ls_sort=ls.copy()
-
 mergesort(ls_sort)
 
-print(ls)
-print(ls_sort)
+print("orig list:", ls)
+print("sort list:", ls_sort)
