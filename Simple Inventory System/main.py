420be5de143cc559c49b051cef2c9627f6a3fca2
vvanden <vvanden@uwo.ca>
2024-02-10 11:37:38 -0500

    Update main.py

--- Simple Inventory System/main.py
+++ Simple Inventory System/main.py
@@ -4,11 +4,13 @@ class Inventory:
         self.price = price
         self.quantity = quantity
 
+    def update_qty(self, quantity):
+        self.quantity = quantity
+
 def find_item_by_name(name):
-    for name in inv:
-        if inv.name == name:
-            found_items.append(item)
-        return found_items
+    for item in inv:
+        if item.name == name:
+            return item
     return None
 
 inv = []
@@ -22,16 +24,60 @@ inv.append(Inventory("Yogurt", 2.99, 3))
 inv.append(Inventory("Milk", 5.99, 4))
 
 while req_active:
-    req = input("(D)isplay product information, (U)pdate quantities, (C)alculate total inventory value, (E)xit: ").lower()
-    if req == "d":
+    req = input("(A)dd product, (D)isplay product information, (U)pdate quantities, (T)otal inventory value, (E)xit: ").lower()
+    if req == "a":
+        print("**ADD PRODUCT**")
+        while True:
+            try:
+                name = str(input("Name: "))
+            except name == "":
+                print("Sorry, I didn't understand that.")
+                continue
+            else:
+                break
+        while True:
+            try:
+                price = float(input("Price: "))
+            except ValueError:
+                print("Sorry, I didn't understand that.")
+                continue
+            else:
+                break
+        while True:
+            try:
+                quantity = int(input("Quantity: "))
+            except ValueError:
+                print("Sorry, I didn't understand that.")
+                continue
+            else:
+                break
+        inv.append(Inventory(name, price, quantity))
+    elif req == "d":
+        print("**DISPLAY PRODUCT INFORMATION**")
         for item in inv:
-            print(f"Name: {item.name}, Price: {item.price}, Quantity: {item.quantity}")
+            print(f"Name: {item.name}, Price: $ {item.price}, Quantity: {item.quantity}")
     elif req == "u":
-        pass
-    elif req == "c":
-        pass
+        print("**UPDATE QUANTITY**")
+        item_name = str(input ("Item Name: "))
+        item = find_item_by_name(item_name)
+        if item:
+            while True:
+                try:
+                    item_quantity = int(input ("Item Quantity:" ))
+                except ValueError:
+                    print("Sorry, I didn't understand that.")
+                    continue
+                else:
+                    break
+            item.update_qty(item_quantity)
+        else:
+            print(f"Item with name '{item_name}' not found.")
+    elif req == "t":
+        print("**TOTAL INVENTORY VALUE**")
+        inv_value = round(sum(item.price * item.quantity for item in inv),2)
+        print("Total Inventory Value: $", inv_value)
     elif req == "e":
-        print("Good bye.")
+        print("**EXIT**")
         req_active = False
     else:
         print("Sorry, I didn't understand that.") 
